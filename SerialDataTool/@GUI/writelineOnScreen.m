0459bc49a12100facfb53f849a4b98c0cea60585
Lukas Kroll <lukas.kroll@fh-dortmund.de>
2022-05-20 09:44:14 +0200

    recordData file change
    cell array to int array

--- SerialDataTool/@GUI/writelineOnScreen.m
+++ SerialDataTool/@GUI/writelineOnScreen.m
@@ -2,11 +2,11 @@ function  writelineOnScreen(obj,str)
    
     ascii = sprintf("%c",str);
 
-    obj.ASCII_ARR(end) = append(obj.ASCII_ARR(end),ascii);
+    obj.ASCII_ARR = append(obj.ASCII_ARR(end),ascii);
     
-    obj.HEX_ARR(end) = append(obj.HEX_ARR(end),ascii);
+    obj.HEX_ARR = append(obj.HEX_ARR(end),ascii);
     
-    obj.NUM_ARR(end) = append(obj.NUM_ARR(end),ascii);
+    obj.NUM_ARR = append(obj.NUM_ARR(end),ascii);
     
 end
 
--- SerialDataTool/@SerialDataTool/recordData.m
+++ SerialDataTool/@SerialDataTool/recordData.m
@@ -3,6 +3,7 @@ function recordData(obj,~,~)
         obj.myGui.btn_recordData.Text = 'Stop Record';
         obj.rd_filename = ['recordfiles/',obj.myGui.txa_inputFileName.Value,'_', char(datetime),'.mat'];
         tmp = obj.rd_Buffer;
+        tmp = cell2mat(tmp);
         save (obj.rd_filename,'tmp');
         obj.RECORD_DATA = 1;
     else
